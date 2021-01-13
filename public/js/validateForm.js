097f27ed816c285c4df815307ffea19fd5548cdd
Sushanth Sk <email>
2021-01-13 19:12:40 +0530

    Updated style

--- public/js/validateForm.js
+++ public/js/validateForm.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
-const sendMessage = document.querySelector('.sendMessage');
+const sendMessage = document.querySelector('.send-message');
 const showError = (errorSpan, text) => {
   errorSpan.textContent = text;
   errorSpan.classList.remove(errorSpan.classList[1]);
   errorSpan.classList.add('show-error');
 
-  sendMessage.classList.remove(sendMessage.classList[3]);
+  // sendMessage.classList.remove(sendMessage.classList[3]);
 };
 const hideError = (errorSpan) => {
   // const sendMessage =  document.querySelector('.sendMessage');
   errorSpan.classList.remove(errorSpan.classList[1]);
   errorSpan.classList.add('hide-error');
-  sendMessage.classList.add('send-response');
+  // sendMessage.classList.add('send-response');
 };
 
 const validateForm = (data, letters, idName, attr) => {
@@ -43,7 +43,6 @@ const validateForm = (data, letters, idName, attr) => {
 };
 
 export const validate = (e) => {
-  let text;
   const name = document.getElementById('name').value.trim();
   const email = document.getElementById('email').value.trim();
   const subject = document.getElementById('subject').value.trim();
@@ -51,9 +50,17 @@ export const validate = (e) => {
   e.preventDefault();
   const nameLetter = /^[A-Za-z ]+$/;
   const subjectLetter = /^[A-Za-z0-9\\,\\.\\; ]+$/;
+  const messageLetter = /^[A-Za-z0-9\\,\\.\\;\\!\\#\\$\\%\\&\\'\\*\\+\\/\\=\\?\\^\\_\\`\\'\\" ]+$/;
   const emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   validateForm(name, nameLetter, 'name', 1);
   validateForm(email, emailFormat, 'email', 2);
   validateForm(subject, subjectLetter, 'subject', 3);
-  validateForm(messge, subjectLetter, 'message', 4);
+  validateForm(messge, messageLetter, 'message', 4);
+
+  const errorValidate = document.querySelectorAll('.show-error');
+  if (errorValidate.length === 0) {
+    sendMessage.classList.add('send-response');
+  } else {
+    sendMessage.classList.remove(sendMessage.classList[3]);
+  }
 };
